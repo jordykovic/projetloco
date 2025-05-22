88887f5fb7692f6985b4c9b60708926dd009b416
Thomas Jordy <59070714+jordykovic@users.noreply.github.com>
2025-05-22 17:36:12 +0200

    Update StyledFileManager.ts
    Reduced to 40%

--- components/system/Files/Views/Icon/StyledFileManager.ts
+++ components/system/Files/Views/Icon/StyledFileManager.ts
@@ -24,7 +24,7 @@ const StyledFileManager = styled.ol<StyledFileManagerProps>`
 
   main > & {
     grid-auto-flow: column;
-    height: calc(50% - ${TASKBAR_HEIGHT}px);
+    height: calc(40% - ${TASKBAR_HEIGHT}px);
     overflow: visible;
     padding-bottom: 21px;
   }
